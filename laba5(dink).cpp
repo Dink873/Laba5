5f62a82b5cb024f96f9b9f056813b3cac2a3062d
Dink <diana.dovbeta4102@gmail.com>
2022-11-30 16:37:30 +0200

    adding laba5(dink).cpp

---
+++ laba5(dink).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+#include "Flower.h"
+#include "FlowerShop.h"
+#include "Bouquet.h"
+
+using namespace std;
+
+int main() {
+	Flower Rose(32, 2, "pink", 320, 3, 5), Romashka(6, 0.1, "white", 2, 10, 3), Blue(11, 0.8, "blue", 210, 6, 4);
+	Bouquet one("one"), two("one");
+	one.add_flower_to_bouquet(Rose);
+	one.add_flower_to_bouquet(Romashka);
+	two.add_flower_to_bouquet(Blue);
+	two.add_flower_to_bouquet(Romashka);
+	cout << one.full_price() << endl;
+	cout << two.full_price() << endl;
+}
